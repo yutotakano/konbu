d344d146b85220c26ecca5892070bef343a3d4f5
Yuto Takano <moa17stock@gmail.com>
2020-11-14 00:38:19 +0000

    Vastly improve prediction quality through a new processing algorithm (took ages to figure out lol)

--- predict.py
+++ predict.py
@@ -9,6 +9,18 @@ from keras.models import load_model
 
 chars = 'あいうえおかきくけこさしすせそたちつてとなにのひふへまみむもやゆよらりん';
 
+def crop_minAreaRect(img, points):
+    highest_point = min([point[1] for point in points])
+    lowest_point = max([point[1] for point in points])
+    leftmost_point = min([point[0] for point in points])
+    rightmost_point = max([point[0] for point in points])
+
+    # rgb = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
+    # cv2.imshow('image2', cv2.rectangle(rgb, (leftmost_point, lowest_point), (rightmost_point, highest_point), (0, 255, 255), 1))
+    # cv2.waitKey(1500)
+
+    return img[highest_point:lowest_point + 1, leftmost_point:rightmost_point + 1]
+    
 def main():
     
     if len(sys.argv) != 2:
@@ -25,24 +37,60 @@ def main():
         nparray = np.array(bytes, dtype=np.uint8)
     data = cv2.imdecode(nparray, cv2.IMREAD_GRAYSCALE)
     if data is None:
-        exit('Image is None.')
-    
-    # data = cv2.resize(data, (24, 24), interpolation=cv2.INTER_AREA)
-    # data = np.reshape(data, (24, 24, 1))
-    
+        exit('Image is None.')    
 
     model = load_model(os.path.dirname(os.path.abspath(__file__)) + '/model.h5')
 
-    ret, threshed = cv2.threshold(data, 175, 255, cv2.THRESH_BINARY)
+    ret, threshed = cv2.threshold(data, 180, 255, cv2.THRESH_BINARY)
 
-    crop_rows = threshed[~np.all(threshed == 255, axis=1), :]
-    cropped_image = crop_rows[:, ~np.all(crop_rows == 255, axis=0)]
-    padded = np.pad(cropped_image, ((0, 2), (2, 0)), 'constant')
+    threshed = np.pad(threshed, ((1,1), (1,1)), 'constant', constant_values=255)
+    contours, hierarchy = cv2.findContours(threshed, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    # cv2.namedWindow('image', cv2.WINDOW_NORMAL)
+    # cv2.resizeWindow('image', 720,240)
+    # cv2.namedWindow('image2', cv2.WINDOW_NORMAL)
+    # cv2.resizeWindow('image2', 720,240)
+    contours.sort(key=(lambda x: cv2.boundingRect(x)[0])) 
+    # rgb = cv2.cvtColor(threshed, cv2.COLOR_GRAY2RGB)
+    temp_points = [] # cumulative list of points for characters
+    letters_data = []
+    for index, c in enumerate(contours[1:]): # include last one, but don't do anything on it
+        if index != len(contours[1:]) - 1:
+            x, y, w, h = cv2.boundingRect(c)
 
-    letters_data = [padded[:, :15 + 2],
-                    padded[:, 15 - 2:30 + 2],
-                    padded[:, 30 - 2:45 + 2],
-                    padded[:, 45 - 2:]]
+            # Next bounding box
+            n_x, n_y, n_w, n_h = cv2.boundingRect(contours[1:][index + 1])
+
+            temp_points.append((x, y))
+            temp_points.append((x+w, y+h))
+            
+            # cv2.imshow('image', cv2.rectangle(rgb, (x,y), (x+w, y+h), (255, 255, 0), 1))
+            # cv2.waitKey(1000)
+        if (
+            n_x + n_w - x >= 16 or
+            index == len(contours[1:]) - 1  # or last one 
+        ):
+            if not temp_points:
+                continue
+            cropped = crop_minAreaRect(threshed, temp_points)
+            (cropped_h, cropped_w) = np.shape(cropped)
+            croppeds = []
+            if cropped_w > 24:
+                croppeds.append(cropped[:, :round(cropped_w/2)])
+                croppeds.append(cropped[:, cropped_w-round(cropped_w/2):])
+            else:
+                croppeds.append(cropped)
+            
+            for crop in croppeds:
+                (crop_h, crop_w) = np.shape(crop)
+                # cv2.imshow('aa', crop)
+                # cv2.waitKey(2000)
+                pad_t = round ((24 - crop_h) / 2)
+                pad_b = 24 - crop_h - pad_t
+                pad_l = round ((24 - crop_w) / 2)
+                pad_r = 24 - crop_w - pad_l
+                padded = np.pad(crop, ((pad_t, pad_b), (pad_l, pad_r)), 'constant', constant_values=255)
+                letters_data.append(padded)
+            temp_points = []
     
     output = ''
     for letter_data in letters_data:
