185bbdb9293ddd0599ff658cc9cf6a6a01e9e192
Yuto Takano <moa17stock@gmail.com>
2020-11-30 15:52:50 +0000

    Make end-of-character detection better, also output max 4 chars

--- predict.py
+++ predict.py
@@ -54,52 +54,63 @@ def main():
     temp_points = [] # cumulative list of points for characters
     letters_data = []
     for index, c in enumerate(contours[1:]): # include last one, but don't do anything on it
-        if index != len(contours[1:]) - 1:
-            x, y, w, h = cv2.boundingRect(c)
+        x, y, w, h = cv2.boundingRect(c)
 
+        temp_points.append((x, y))
+        temp_points.append((x+w, y+h))
+        
+        # cv2.imshow('image', cv2.rectangle(rgb, (x,y), (x+w, y+h), (255, 255, 0), 1))
+        # cv2.waitKey(1000)
+
+        if not temp_points:
+            continue
+
+        if index != len(contours[1:]) - 1:
             # Next bounding box
             n_x, n_y, n_w, n_h = cv2.boundingRect(contours[1:][index + 1])
-
-            temp_points.append((x, y))
-            temp_points.append((x+w, y+h))
-            
-            # cv2.imshow('image', cv2.rectangle(rgb, (x,y), (x+w, y+h), (255, 255, 0), 1))
-            # cv2.waitKey(1000)
-        if (
-            n_x + n_w - x >= 16 or
-            index == len(contours[1:]) - 1  # or last one 
-        ):
-            if not temp_points:
-                continue
-            cropped = crop_minAreaRect(threshed, temp_points)
-            (cropped_h, cropped_w) = np.shape(cropped)
-            croppeds = []
-            if cropped_w > 24:
-                croppeds.append(cropped[:, :round(cropped_w/2)])
-                croppeds.append(cropped[:, cropped_w-round(cropped_w/2):])
-            else:
-                croppeds.append(cropped)
-            
-            for crop in croppeds:
-                (crop_h, crop_w) = np.shape(crop)
-                # cv2.imshow('aa', crop)
-                # cv2.waitKey(2000)
-                pad_t = round ((24 - crop_h) / 2)
-                pad_b = 24 - crop_h - pad_t
-                pad_l = round ((24 - crop_w) / 2)
-                pad_r = 24 - crop_w - pad_l
-                padded = np.pad(crop, ((pad_t, pad_b), (pad_l, pad_r)), 'constant', constant_values=255)
-                letters_data.append(padded)
-            temp_points = []
     
-    output = ''
+            end_of_newpoint_within_some_previous = any([(n_x + n_w) < p[0] for p in temp_points])
+            min_of_temps_x = min([item[0] for item in temp_points])
+            if end_of_newpoint_within_some_previous or (n_x + n_w - min_of_temps_x <= 19):
+                continue
+
+        cropped = crop_minAreaRect(threshed, temp_points)
+        (cropped_h, cropped_w) = np.shape(cropped)
+        croppeds = []
+        if cropped_w > 24:
+            croppeds.append(cropped[:, :round(cropped_w/2)])
+            croppeds.append(cropped[:, cropped_w-round(cropped_w/2):])
+        else:
+            croppeds.append(cropped)
+        
+        for crop in croppeds:
+            (crop_h, crop_w) = np.shape(crop)
+            pad_t = round ((24 - crop_h) / 2)
+            pad_b = 24 - crop_h - pad_t
+            pad_l = round ((24 - crop_w) / 2)
+            pad_r = 24 - crop_w - pad_l
+            padded = np.pad(crop, ((pad_t, pad_b), (pad_l, pad_r)), 'constant', constant_values=255)
+            letters_data.append(padded)
+            # cv2.imshow('aa', padded)
+            # cv2.waitKey(2000)
+        temp_points = []
+    
+    output = []
     for letter_data in letters_data:
         letter_data = cv2.resize(letter_data, (24, 24))
         letter_data = np.reshape(letter_data, (24, 24, 1))
         letter_data = np.array([letter_data], dtype=float) / 255.0
-        output += chars[np.argmax(np.round(model.predict(letter_data), 3))]
+        percentages = np.round(model.predict(letter_data), 3)
+        output.append((chars[np.argmax(percentages)], np.max(percentages)))
     
-    print(output)
+    if len(output) > 4:
+        output_copy = [a for a in output]
+        output_copy.sort(key=lambda x: (-1)*x[1])
+        fourth_largest = output_copy[3][1]
+
+        output = filter(lambda x: x[1] >= fourth_largest, output)
+
+    print(''.join([a[0] for a in output]))
 
 if __name__ == "__main__":
     main()
